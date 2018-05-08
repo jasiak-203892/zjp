e0d2f967bc63c355dd90974e0b84fcce5efd12f2
jasiak-203892 <jasio111996@gmail.com>
2018-05-08 19:14:51 +0200

    Initial commit

---
+++ zjp/README.md
@@ -0,0 +1,14 @@
+## Zaawansowana Java
+
+#### Zrobione
+- Git
+- GitHub
+- Maven
+- Spring Boot
+
+#### Zespół
+- Martyna Jasiak
+- Kasia Meckier
+- Marta Stempel
+- Paweł Wiktorek
+- Piotr Paprzycki
---
+++ zjp/src/main/java/com/app/zjp/ZjpApplication.java
@@ -0,0 +1,12 @@
+package com.app.zjp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZjpApplication.class, args);
+	}
+}
