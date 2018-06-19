53e519e73d48daa7cd91eda4a6018ba88df463a7
paprzycki-189745 <189745@edu.p.lodz.pl>
2018-06-20 01:11:34 +0200

    Change StudentRepository as CrudRepository

--- zjp/src/main/java/com/app/zjp/restservice/repository/StudentRepository.java
+++ zjp/src/main/java/com/app/zjp/restservice/repository/StudentRepository.java
@@ -1,10 +1,10 @@
 package com.app.zjp.restservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.app.zjp.restservice.entity.Student;
 
-@RepositoryRestResource(path = "/students")
-public interface StudentRepository extends JpaRepository<Student, Integer> {
+public interface StudentRepository extends CrudRepository<Student, Integer> {
 }
