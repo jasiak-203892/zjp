b009f8a4f41a4a5cc8a9aaf37b8d91b110bb0d28
paprzycki-189745 <189745@edu.p.lodz.pl>
2018-06-20 03:38:48 +0200

    Add /class/<id>/add-student?studentId=<studentId>

--- zjp/src/main/java/com/app/zjp/restservice/controller/ClassGroupController.java
+++ zjp/src/main/java/com/app/zjp/restservice/controller/ClassGroupController.java
@@ -2,20 +2,25 @@ package com.app.zjp.restservice.controller;
 
 import com.app.zjp.restservice.entity.ClassGroup;
 import com.app.zjp.restservice.entity.Student;
+import com.app.zjp.restservice.entity.Subject;
+import com.app.zjp.restservice.entity.Teacher;
 import com.app.zjp.restservice.repository.ClassGroupRepository;
 import com.app.zjp.restservice.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.logging.Logger;
 
 @Controller
 @RequestMapping(path="/class")
 public class ClassGroupController {
     @Autowired
     private ClassGroupRepository classGroupRepositoy;
+
+    @Autowired
+    private StudentRepository studentRepository;
 
     @GetMapping(path="/add")
     public @ResponseBody
@@ -24,6 +29,35 @@ public class ClassGroupController {
         n.setName(name);
         classGroupRepositoy.save(n);
         return "Saved";
+    }
+
+    @GetMapping(path="/{id}/add-student")
+    public @ResponseBody
+    String addStudentToClass (@PathVariable("id") Integer id, @RequestParam Integer studentId) {
+        Optional<ClassGroup> option = classGroupRepositoy.findById(id);
+        if (option.isPresent()){
+            Optional<Student> subjectOption = studentRepository.findById(studentId);
+            if (subjectOption.isPresent()){
+                ClassGroup classGroup = option.get();
+                Student student = subjectOption.get();
+
+                Logger logger = Logger.getLogger("he");
+
+                logger.info(String.format("%d, %d", id, studentId));
+
+                if (!classGroup.getStudents().contains(student)){
+                    student.setClassGroup(classGroup);
+                    studentRepository.save(student);
+                } else {
+                    return "Class already has student with this id!";
+                }
+            } else {
+                return String.format("Student with id %d does not exists!", studentId);
+            }
+        } else {
+            return String.format("Class with id %d does not exists!", id);
+        }
+        return "Added!";
     }
 
     @GetMapping(path="/all")
