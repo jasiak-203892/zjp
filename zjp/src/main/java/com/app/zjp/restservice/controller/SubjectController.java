932e3e3641a0692471211ab9540a0f0bc024e1de
204028 Wiktorek Paweł <204028@edu.p.lodz.pl>
2018-06-20 14:35:01 +0200

    Add subject controller, delete options to Teacher, Student

---
+++ zjp/src/main/java/com/app/zjp/restservice/controller/SubjectController.java
@@ -0,0 +1,51 @@
+package com.app.zjp.restservice.controller;
+
+import java.util.Optional;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.app.zjp.restservice.entity.Subject;
+import com.app.zjp.restservice.repository.SubjectRepository;
+
+@Controller
+@RequestMapping(path="/subjects")
+public class SubjectController {
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    @GetMapping(path="/add")
+    public @ResponseBody
+    String addNewSubject (@RequestParam String name) {
+        Subject subject = new Subject();
+        subject.setName(name);
+        subjectRepository.save(subject);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Subject> getAllSubjects() {
+        return subjectRepository.findAll();
+    }
+    @GetMapping(path="/delete")
+    public @ResponseBody
+    String deleteTeacher (@RequestParam Integer id) {
+        Subject subjectToDelete = new Subject();
+        Optional<Subject> option = subjectRepository.findById(id);
+        if(option.isPresent()) {
+        	subjectToDelete=option.get();
+        	subjectRepository.delete(subjectToDelete);
+        }
+        else {
+        	return "No subject with this ID";
+        }
+
+        return "Subject has been deleted";
+    }
+}
--- zjp/src/main/java/com/app/zjp/restservice/repository/SubjectRepository.java
+++ zjp/src/main/java/com/app/zjp/restservice/repository/SubjectRepository.java
@@ -2,8 +2,9 @@ package com.app.zjp.restservice.repository;
 
 import com.app.zjp.restservice.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "/subjects")
-public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+public interface SubjectRepository extends CrudRepository<Subject, Integer> {
 }
