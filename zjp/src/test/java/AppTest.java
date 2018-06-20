d5a6c44e1967881dbd6974b2417d62807863fb15
LENOVO\Meckier <203941@edu.p.lodz.pl>
2018-06-20 09:29:10 +0200

    Add bodyTest

--- zjp/src/test/java/AppTest.java
+++ zjp/src/test/java/AppTest.java
@@ -1,3 +1,5 @@
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.given;
@@ -10,5 +12,14 @@ public class AppTest {
         given().when().get("http://localhost:8090/students").then().statusCode(200);
     }
 
+    @Test
+    public void bodyTest() {
+
+        Response resp = RestAssured.get("http://localhost:8090/students");
+
+        String data = resp.asString();
+
+        System.out.println("Data is " + data);
+    }
 
 }
