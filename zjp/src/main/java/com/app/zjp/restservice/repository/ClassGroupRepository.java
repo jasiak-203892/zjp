fba1bb1762ec5ed98b3d034af58ffe9d6bc02097
paprzycki-189745 <189745@edu.p.lodz.pl>
2018-06-20 03:14:13 +0200

    Add ClassGroupRepository

---
+++ zjp/src/main/java/com/app/zjp/restservice/repository/ClassGroupRepository.java
@@ -0,0 +1,8 @@
+package com.app.zjp.restservice.repository;
+
+import com.app.zjp.restservice.entity.ClassGroup;
+import com.app.zjp.restservice.entity.Student;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClassGroupRepository extends CrudRepository<ClassGroup, Integer> {
+}
