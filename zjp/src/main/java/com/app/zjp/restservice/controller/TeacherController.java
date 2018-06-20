46043015eb73ef45a8c63cc0ebfc9752d3d39902
paprzycki-189745 <189745@edu.p.lodz.pl>
2018-06-20 03:03:36 +0200

    Add /teacher/<id>/add-subject/<subjectId> endpoint

--- zjp/src/main/java/com/app/zjp/restservice/controller/TeacherController.java
+++ zjp/src/main/java/com/app/zjp/restservice/controller/TeacherController.java
@@ -1,19 +1,25 @@
 package com.app.zjp.restservice.controller;
 
+import com.app.zjp.restservice.entity.Subject;
 import com.app.zjp.restservice.entity.Teacher;
+import com.app.zjp.restservice.repository.SubjectRepository;
 import com.app.zjp.restservice.repository.TeachertRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path="/teacher")
 public class TeacherController {
     @Autowired
     private TeachertRepository teacherRepository;
+
+    @Autowired
+    private SubjectRepository subjectRepository;
 
     @GetMapping(path="/add")
     public @ResponseBody
@@ -23,6 +29,31 @@ public class TeacherController {
         n.setEmail(email);
         teacherRepository.save(n);
         return "Saved";
+    }
+
+    @GetMapping(path="/{id}/add-subject")
+    public @ResponseBody
+    String addSubjectToTeacher (@PathVariable("id") Integer id, @RequestParam Integer subjectId) {
+        Optional<Teacher> option = teacherRepository.findById(id);
+        if (option.isPresent()){
+            Optional<Subject> subjectOption = subjectRepository.findById(subjectId);
+            if (subjectOption.isPresent()){
+                Teacher teacher = option.get();
+                Subject subject = subjectOption.get();
+
+                if (!teacher.getSubjects().contains(subject)){
+                    teacher.getSubjects().add(subject);
+                    teacherRepository.save(teacher);
+                } else {
+                    return "Teacher already has subject with this id!";
+                }
+            } else {
+                return String.format("Subject with id %d does not exists!", subjectId);
+            }
+        } else {
+            return String.format("Teacher with id %d does not exists!", id);
+        }
+        return "Added!";
     }
 
     @GetMapping(path="/all")
