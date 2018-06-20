5d75bcbe590a18467c59b7f0cafada82a85b8173
204028 Wiktorek Paweł <204028@edu.p.lodz.pl>
2018-06-20 16:02:21 +0200

    add Grades to Subject

--- zjp/src/main/java/com/app/zjp/restservice/entity/Subject.java
+++ zjp/src/main/java/com/app/zjp/restservice/entity/Subject.java
@@ -1,9 +1,13 @@
 package com.app.zjp.restservice.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Subject {
@@ -12,8 +16,18 @@ public class Subject {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String name;
+    @OneToMany(targetEntity=Grade.class)
+    private ArrayList<Grade> grades;
 
-    public Integer getId() {return id; }
+    public ArrayList<Grade> getGrades() {
+		return grades;
+	}
+
+	public void setGrades(ArrayList<Grade> grades) {
+		this.grades = grades;
+	}
+
+	public Integer getId() {return id; }
 
     public void setId(Integer id) {
         this.id = id;
